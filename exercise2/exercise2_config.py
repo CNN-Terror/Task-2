7f9f033413826f59406d482afe0da496ec056e1e
Sophie Crevoiserat <sophie.crevoiserat@unifr.ch>
2022-04-08 16:49:01 +0200

    Conversion from png permutated data to csv and modify option for it on CNN

--- exercise2/exercise2_config.py
+++ exercise2/exercise2_config.py
@@ -1,2 +1,5 @@
 TRAIN_DATA_FILE = "../mnist/mnist-csv-format/mnist_train.csv"
 TEST_DATA_FILE = "../mnist/mnist-csv-format/mnist_test.csv"
+
+TRAIN_PERMUTATED_DATA_FILE = "../mnist/mnist-png-format/train"
+TEST_PERMUTATED_DATA_FILE = "../mnist/mnist-png-format/test"
--- exercise2/readers.py
+++ exercise2/readers.py
@@ -1,11 +1,14 @@
 import csv
 from image import Image
+from PIL import Image
+import numpy as np
+import os
+import exercise2_config as config
 
 
 # Reads a list of Images from the given csv file.
 def ReadFromCsvFile(filename):
   images = []
-
   with open(filename, 'r') as file:
     csv_reader = csv.reader(file, delimiter=',')
     for row in csv_reader:
@@ -13,10 +16,33 @@ def ReadFromCsvFile(filename):
       image.label = int(row[0])
       image.pixels = [int(pixel) for pixel in row[1:]]
       images.append(image)
-  
   return images
 
+def ReadImages(dir, label):
+  images = []
+  print(dir)
+  for filename in os.listdir(dir) :
+    image_path = os.path.join(dir, filename)
+    image = Image.open(image_path)
+    image_as_array = np.array(image.getdata())
+    images.append(image_as_array)
+  images = np.insert(images, 0, label, axis=1) 
+  return images
 
 # Reads an Image from the given png file.
-def ReadFromPngFile(filename):
-  pass
+def ReadFromPngFiles():
+  for label in range(10):
+    dirTrain = os.path.join(config.TRAIN_PERMUTATED_DATA_FILE, f'{label}')
+    dirTest = os.path.join(config.TEST_PERMUTATED_DATA_FILE, f'{label}')
+    imagesTrain = ReadImages(dirTrain, label)
+    imagesTest = ReadImages(dirTest, label)
+    #Delete the output.csv file after each run, because it appends the result at the end of it each time
+    with open("../mnist/trainOutput.csv", "a") as f1:
+      writer = csv.writer(f1)
+      writer.writerows(imagesTrain)
+    with open("../mnist/testOutput.csv", "a") as f2:
+      writer = csv.writer(f2)
+      writer.writerows(imagesTest)
+    #input() 
+  f1.close()
+  f2.close()
