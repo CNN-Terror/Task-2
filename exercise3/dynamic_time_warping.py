190cb8ba86b265982c1b7321def9413e986834c0
Sophie Crevoiserat <sophie.crevoiserat@unifr.ch>
2022-04-25 22:29:56 +0200

    DTW with window constraint

--- exercise3/dynamic_time_warping.py
+++ exercise3/dynamic_time_warping.py
@@ -1,23 +1,25 @@
 # Great YT video + code: https://github.com/kamperh/lecture_dtw_notebook
 import numpy as np
 
-# TODO: Sakoe-Chiba Band: Reduce the number of paths to consider
+#If too slow, maybe try with this library: https://pypi.org/project/fastdtw/
 
-#Dissimilarity between two feature vector sequences
-#Dynamic time warping aligns two sequences, along a common time axis usually with Euclidean cost
+#Dynamic time warping: Align two sequences, along a common time axis with Euclidean cost
+#Sakoe-Chiba Band: Reduce the number of paths to consider in DTW by adding a window constraint
 #https://towardsdatascience.com/dynamic-time-warping-3933f25fcdd
-def dtw(vector1, vector2):
+def dtwWindow(vector1, vector2, window):
     n, m = len(vector1), len(vector2)
     dtw = np.array(np.ones((n+1, m+1)) * np.inf)
-    dtw[0][0] = 0  
+    dtw[0][0] = 0 
+
+    #Adapt window size if necessary 
+    window = np.max([window, np.abs(n-m)])
+
     for i in range(1, n+1):
-        for j in range(1, m+1):
+        for j in range(np.max([1, i-window]), np.min([m, i+window])+1):
+            dtw[i][j] = 0
+            
+    for i in range(1, n+1):
+        for j in range(np.max([1, i-window]), np.min([m, i+window])+1):
             cost = abs(vector1[i-1]-vector2[j-1])
             dtw[i, j] = cost + np.min([dtw[i-1, j], dtw[i, j-1], dtw[i-1, j-1]])
-    print(dtw)
     return dtw
-
-#To test
-v1 = [1, 2, 3]
-v2 = [2, 2, 2, 3, 4]
-dtw(v1, v2)
