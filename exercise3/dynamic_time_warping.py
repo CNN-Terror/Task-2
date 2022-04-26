b095579d27b852e86fea3ac995a0a3e7a2efaca7
Sophie Crevoiserat <sophie.crevoiserat@unifr.ch>
2022-04-26 15:00:23 +0200

    Add different types of outputs for DTW

--- exercise3/dynamic_time_warping.py
+++ exercise3/dynamic_time_warping.py
@@ -3,6 +3,21 @@ import numpy as np
 
 #If too slow, maybe try with this library: https://pypi.org/project/fastdtw/
 
+#To get the path from cost matrix: https://towardsdatascience.com/an-intuitive-approach-to-dtw-dynamic-time-warping-f660ccb77ff4
+def trace_path(cost_matrix, i, j):
+    path = [(i, j)]
+    while ((i>0) | (j>0)):
+        path_min = np.argmin([cost_matrix[i-1, j-1], cost_matrix[i-1, j], cost_matrix[i, j-1]])   
+        if path_min==0:
+            i=i-1
+            j=j-1
+        elif path_min==1:
+            i=i-1
+        else:
+            j=j-1
+        path.append((i, j)) 
+    return path[::-1] ## return after reversing the list
+
 #Dynamic time warping: Align two sequences, along a common time axis with Euclidean cost
 #Sakoe-Chiba Band: Reduce the number of paths to consider in DTW by adding a window constraint
 #https://towardsdatascience.com/dynamic-time-warping-3933f25fcdd
@@ -22,4 +37,6 @@ def dtwWindow(vector1, vector2, window):
         for j in range(np.max([1, i-window]), np.min([m, i+window])+1):
             cost = abs(vector1[i-1]-vector2[j-1])
             dtw[i, j] = cost + np.min([dtw[i-1, j], dtw[i, j-1], dtw[i-1, j-1]])
-    return dtw
+    finalCost = dtw[n][m]
+    path = trace_path(dtw, n, m)
+    return dtw, finalCost, path #Return the matrix cost, the final cost and the best path to take
