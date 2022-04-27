9adb03480dc41d3f7c58798fbe7278ccd2212e2d
[Emile Gerber] <[emile.gerber@unine.ch]>
2022-04-27 21:00:12 +0200

    Now list of features vectors

--- exercise3/feature_extraction.py
+++ exercise3/feature_extraction.py
@@ -14,90 +14,106 @@ class Method(Enum):
 #  - methods: list of methods (features we want to extract),
 #         ex: methods = [Method.BLACK_PIXEL_RATIO, Method.LOWER_CONTOUR]
 # Output:
-#  - vector of normalized features (using min-max normalization)
+#  - list of vectors of normalized features (using min-max normalization)
+#     where each vector is composed of features for a pixels column
 def ExtractFeatures(image, methods):
-  features_vector = []
-  for method in methods:
-    if method == Method.BLACK_PIXEL_RATIO:
-      features_vector.append(__ExtractBlackPixelRatio(image))
-    if method == Method.UPPER_CONTOUR:
-      features_vector.append(__ExtractUpperContour(image)/image.height)
-    if method == Method.LOWER_CONTOUR:
-      features_vector.append((__ExtractLowerContour(image) + 1)/image.height)
-    if method == Method.PIXEL_TRANSITIONS:
-      features_vector.append(__ExtractPixelTransitions(image)/(image.height*image.width - 1))
-    if method == Method.BLACK_PIXEL_RATIO_LC_UP:
-      features_vector.append(__ExtractBlackPixelRatioLcUp(image))
+  decomposed_image_with_features = []
+  for column in range(image.width):
+    features_vector = []
+    for method in methods:
+      if method == Method.BLACK_PIXEL_RATIO:
+        features_vector.append(__ExtractBlackPixelRatio(image, column))
+      if method == Method.UPPER_CONTOUR:
+        features_vector.append(__ExtractUpperContour(image, column)/image.height)
+      if method == Method.LOWER_CONTOUR:
+        features_vector.append((__ExtractLowerContour(image, column) + 1)/image.height)
+      if method == Method.PIXEL_TRANSITIONS:
+        features_vector.append(__ExtractPixelTransitions(image, column)/(image.height - 1))
+      if method == Method.BLACK_PIXEL_RATIO_LC_UP:
+        features_vector.append(__ExtractBlackPixelRatioLcUp(image, column))
 
-  return features_vector
+    decomposed_image_with_features.append(features_vector)
+  return decomposed_image_with_features
   pass
 
 # Input:
-#  - image: PIL.Image 
+#  - image: PIL.Image
+#  - column: index of the column we are investigating
 # Output:
 #  - number of black pixels/total number of pixels
-def __ExtractBlackPixelRatio(image):
+def __ExtractBlackPixelRatio(image, column):
   text_color_rgb = sorted(list(dict.fromkeys(image.getcolors())), key=lambda x: x[1])
-  nb_of_black_pixels, _ = text_color_rgb[0]
+  _, text_color = text_color_rgb[0]
+  nb_of_black_pixels = 0
 
-  return nb_of_black_pixels/(image.height * image.width)
+  for row in range(image.height):
+    if image.getpixel((column, row)) == text_color:
+      nb_of_black_pixels += 1
+  return nb_of_black_pixels/image.height
 
 # Input:
 #  - image: PIL.Image
+#  - column: index of the column we are investigating
 # Output:
 #  - row index of the highest black pixel
-def __ExtractUpperContour(image):
+def __ExtractUpperContour(image, column):
   text_color_rgb = sorted(list(dict.fromkeys(image.getcolors())), key=lambda x: x[1])
   _, text_color = text_color_rgb[0]
 
   for row in range(image.height):
-    for column in range(image.width):
-      if image.getpixel((column, row)) == text_color:
-        return row
+    if image.getpixel((column, row)) == text_color:
+      return row
 
   return image.height
 
 # Input:
 #  - image: PIL.Image
+#  - column: index of the column we are investigating
 # Output:
 #  - row index of the lowest black pixel
-def __ExtractLowerContour(image):
+def __ExtractLowerContour(image, column):
   text_color_rgb = sorted(list(dict.fromkeys(image.getcolors())), key=lambda x: x[1])
   _, text_color = text_color_rgb[0]
 
   for row in range(image.height):
-    for column in range(image.width):
-      if image.getpixel((column, (image.height - 1) - row)) == text_color:
-        return (image.height - 1) - row
+    if image.getpixel((column, (image.height - 1) - row)) == text_color:
+      return (image.height - 1) - row
 
   return -1
 
 # Input:
 #  - image: PIL.Image
+#  - column: index of the column we are investigating
 # Output:
 #  - number of transitions between black and white pixels
-def __ExtractPixelTransitions(image):
+def __ExtractPixelTransitions(image, column):
   text_color_rgb = sorted(list(dict.fromkeys(image.getcolors())), key=lambda x: x[1])
   _, text_color = text_color_rgb[0]
 
   nb_transitions = 0
-  current = image.getpixel((0, 0))
+  current = image.getpixel((column, 0))
 
   for row in range(image.height):
-    for column in range(image.width):
-      if image.getpixel((column, row)) != current:
-        current = image.getpixel((column, row))
-        nb_transitions += 1
+    if image.getpixel((column, row)) != current:
+      current = image.getpixel((column, row))
+      nb_transitions += 1
   return nb_transitions
 
 # Input:
 #  - image: PIL.Image
+#  - column: index of the column we are investigating
 # Output:
 #  - number of black pixels/number of pixels between the upper and the lower contour
-def __ExtractBlackPixelRatioLcUp(image):
+def __ExtractBlackPixelRatioLcUp(image, column):
   text_color_rgb = sorted(list(dict.fromkeys(image.getcolors())), key=lambda x: x[1])
-  nb_of_black_pixels, _ = text_color_rgb[0]
-  lower_contour = __ExtractLowerContour(image)
-  upper_contour = __ExtractUpperContour(image)
-  nb_of_pixels_between = ((lower_contour - upper_contour + 1) * image.width)
+  _, text_color = text_color_rgb[0]
+  nb_of_black_pixels = 0
+
+  for row in range(image.height):
+    if image.getpixel((column, row)) == text_color:
+      nb_of_black_pixels += 1
+  lower_contour = __ExtractLowerContour(image, column)
+  upper_contour = __ExtractUpperContour(image, column)
+  nb_of_pixels_between = (lower_contour - upper_contour + 1)
   return nb_of_black_pixels/nb_of_pixels_between
+
