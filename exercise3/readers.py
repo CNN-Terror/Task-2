c91528009f3287f0dcd140d9a88ce729c2fef030
Adriana Moisil <adriana.moisil@gmail.com>
2022-05-02 13:05:08 +0200

    Extract method that reads and processes input data.

--- exercise3/readers.py
+++ exercise3/readers.py
@@ -1,6 +1,37 @@
 # Methods used to read from the input files exercise.
 import os
 import exercise3_config as config
+from image_preprocessing import CropAllWordImages
+from word_builder import BuildTrainAndTestWords
+from ipynb.fs.full.binarization import ApplyOtsu
+
+
+# Applies binarization on the original image, then crops the words using the given mask.
+def ExtractWordImagesFromOriginalImage(original_image_path, mask_path):
+  image_after_binarization = ApplyOtsu(original_image_path)
+  return CropAllWordImages(image_after_binarization, mask_path)
+
+
+# Extracts all words needed for train and test
+# Output:
+#  - train_words_per_image: dict containing all train words
+#  - testt_words_per_image: dict containing all test words
+# Entries in the output dictionaries are like dict[page_number] = list_of_words
+def ExtractTrainAndTestWords(train_images_numbers, train_jpg_paths, train_svg_paths,
+                             test_images_numbers, test_jpg_paths, test_svg_paths):
+  train_words_per_image = {}
+  # for index in range(len(train_images_numbers)):
+  # TODO: iterate over all images. we can leave it like this for now for testing
+  for index in range(1):
+    train_words_per_image[train_images_numbers[index]] = ExtractWordImagesFromOriginalImage(train_jpg_paths[index], train_svg_paths[index])
+
+  test_words_per_image = {}
+  # for index in range(len(test_images_numbers)):
+  # TODO: iterate over all images. we can leave it like this for now for testing
+  for index in range(1):
+    test_words_per_image[test_images_numbers[index]] = ExtractWordImagesFromOriginalImage(test_jpg_paths[index], test_svg_paths[index])
+
+  return train_words_per_image, test_words_per_image
 
 
 # Output:
@@ -72,3 +103,24 @@ def ExtractKeywords():
       keywords_to_search.append(line.strip())
 
   return keywords_to_search
+
+
+# Reads all input data and processes it.
+def GetProcessedInputData(feature_extraction_methods):
+  transcriptions_as_list = ExtractTranscriptionsAsList()
+  keywords_to_search = ExtractKeywords()
+
+  # Extract paths for train and test files
+  train_images_numbers, train_jpg_paths, train_svg_paths = \
+    ExtractImagePaths(os.path.join(config.DATA_ROOT_DIR, 'task/train.txt'))
+  test_images_numbers, test_jpg_paths, test_svg_paths = \
+    ExtractImagePaths(os.path.join(config.DATA_ROOT_DIR, 'task/valid.txt'))
+
+  train_words_per_image, test_words_per_image = \
+    ExtractTrainAndTestWords(train_images_numbers, train_jpg_paths, train_svg_paths,
+                            test_images_numbers, test_jpg_paths, test_svg_paths)
+
+  train_words, test_words = \
+    BuildTrainAndTestWords(train_words_per_image, test_words_per_image, transcriptions_as_list, feature_extraction_methods)
+  
+  return transcriptions_as_list, keywords_to_search, train_words, test_words
