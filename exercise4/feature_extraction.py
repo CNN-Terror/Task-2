ed665ffd3d16000afc2a4d850d6eadb8e6fd2b5b
Adriana Moisil <adriana.moisil@gmail.com>
2022-05-12 22:36:04 +0200

    Move Feature class to separate file

--- exercise4/feature_extraction.py
+++ exercise4/feature_extraction.py
@@ -1,24 +1,5 @@
 import numpy as np
-
-
-class Feature:
-  def __init__(self, t, x, y, pressure, penup, azimuth, inclination):
-    self.t = t
-    self.x = x
-    self.y = y
-    self.pressure = pressure
-    self.penup = penup
-    self.azimuth = azimuth
-    self.inclination = inclination
-
-
-  t = None
-  x = None
-  y = None
-  pressure = None
-  penup = None
-  azimuth = None
-  inclination = None
+from feature import Feature
 
 
 # Input:
