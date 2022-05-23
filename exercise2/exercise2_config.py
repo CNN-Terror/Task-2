a4d505b676af811df54e640613716aa724e93a63
Adriana Moisil <adriana.moisil@gmail.com>
2022-05-23 10:22:58 +0200

    MLP results for competition

--- exercise2/exercise2_config.py
+++ exercise2/exercise2_config.py
@@ -3,3 +3,8 @@ TEST_DATA_FILE = "../mnist/mnist-csv-format/mnist_test.csv"
 
 TRAIN_PERMUTATED_DATA_FILE = "../mnist/mnist-png-format/train"
 TEST_PERMUTATED_DATA_FILE = "../mnist/mnist-png-format/test"
+
+COMPETITION_FILE = "../competition/mnist_test.csv"
+COMPETITION_OUTPUT_SVM = "../competition/svm.txt"
+COMPETITION_OUTPUT_MLP = "../competition/mlp.txt"
+COMPETITION_OUTPUT_CNN = "../competition/cnn.txt"
